cf57efcdfedb952b09395afad1b783b659e795b1
Shiv Gopal Verma <56026986+shivgopal07@users.noreply.github.com>
2020-08-25 14:04:24 +0530

    Create sliching.py

---
+++ sliching.py
@@ -0,0 +1,25 @@
+#
+#   012345678901234567890123456
+l= "abcdefghijklmnopqrstuvwxyz"
+
+#shiv gopal verma
+print(l[18])
+print(l[7])
+print(l[8])
+print(l[21])
+
+print()
+
+print(l[6])
+print(l[14])
+print(l[15])
+print(l[0])
+print(l[11])
+
+print()
+
+print(l[21])
+print(l[4])
+print(l[17])
+print(l[12])
+print(l[0])
