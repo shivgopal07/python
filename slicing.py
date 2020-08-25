4d53743fc06e502e2eb4b0338dfdb6ef2c8dacc3
Shiv Gopal Verma <56026986+shivgopal07@users.noreply.github.com>
2020-08-25 13:34:56 +0530

    Create slicing.py

---
+++ slicing.py
@@ -0,0 +1,16 @@
+#
+#        01234567891234
+parrot= "Norwegain Blue"
+print(parrot[3:5])
+print(parrot[3])
+print(parrot[7:9])
+ 
+ print()
+ 
+ print(parrot[3-14])
+ print(parrot[4-14])
+ print()
+ print(parrot[3-14])
+ print(parrot[7-14])
+ print(parrot[8-14])
+ 
